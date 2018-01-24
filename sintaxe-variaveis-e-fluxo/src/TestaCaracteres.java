531c6c9abfa39cef86aea993985289bc8f3eed9a
Andre Chaves <andrechavesg@gmail.com>
2018-01-24 16:32:34 -0200

    capitulo5

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,20 @@
+public class TestaCaracteres{
+
+    public static void main(String[] args){
+
+        char letra = 'a';
+        System.out.println(letra);
+
+        char valor = 65;                    // Compila!
+        System.out.println(valor);
+
+        //valor = (char) (valor + 1);         // Não compila!
+        //System.out.println(valor);
+
+        String palavra = "alura cursos online de tecnologia";
+        System.out.println(palavra);
+
+        palavra = palavra + 2020;
+        System.out.println(palavra);
+    }
+}
